8bcca7dc6030af111fed6a89f468fe7206f49862
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-12 01:07:52 -0500

    Add some imports

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { useProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import 'react-pro-sidebar/dist/css/styles.css'
 import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 
 
 const Sidebar = () => {
