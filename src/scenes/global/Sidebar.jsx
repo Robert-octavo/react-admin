fff4534d82449eaae4c329041ac77186c5ee1593
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-11 23:16:57 -0500

    first commit

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,12 @@
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useState } from "react";
+import { useProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css'
+import { Link } from "react-router-dom";
+
+
+const Sidebar = () => {
+    return <div>Sidebar</div>
+}
+
+export default Sidebar;
