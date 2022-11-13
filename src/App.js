29b36d08b7bd0a3ccbef4c35635331bf2d99a4b0
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-13 00:39:43 -0500

    update App.js comment some routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Dashboard from "./scenes/dashboard";
 //import Pie from "./scenes/pie";
 //import FAQ from "./scenes/faq";
 //import Geography from "./scenes/geography";
-//import Form from "./scenes/form";
 //import Calender from "./scenes/calender";
 
 
@@ -29,16 +28,16 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacs />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/calender" element={<Calender />} />
+              {/*<Route path="/team" element={<Team />} />*/}
+              {/*<Route path="/contacts" element={<Contacs />} />*/}
+              {/*<Route path="/invoices" element={<Invoices />} />*/}
+              {/*<Route path="/form" element={<Form />} />*/}
+              {/*<Route path="/bar" element={<Bar />} />*/}
+              {/*<Route path="/pie" element={<Pie />} />*/}
+              {/*<Route path="/line" element={<Line />} />*/}
+              {/*<Route path="/faq" element={<FAQ />} />*/}
+              {/*<Route path="/geography" element={<Geography />} />*/}
+              {/*<Route path="/calender" element={<Calender />} />*/}
             </Routes>
           </main>
         </div>
