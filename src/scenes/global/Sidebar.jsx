57a6f8b1e53777a9e16e87e3f5b2b03f407fb429
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-12 20:58:25 -0500

    Update Sidebard - import icons

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -4,10 +4,26 @@ import { useProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import 'react-pro-sidebar/dist/css/styles.css'
 import { Link } from "react-router-dom";
 import { tokens } from "../../theme";
-import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarCharOutlinedIcon from "@mui/icons-material/BarCharOutlined";
+import PieCharOutlineOutlinedIcon from "@mui/icons-material/PieCharOutlineOutlined";
+import TimeLineOutlinedIcon from "@mui/icons-material/TimeLineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 
 const Sidebar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIscollapse] = useState(false);
+    const [selected, setSelected] = useState("Dashboard");
+    
     return <div>Sidebar</div>
 }
 
