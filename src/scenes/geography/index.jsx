35052c332002c2ba49d7a35d143ca5aa5b0e5815
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-14 17:32:20 -0500

    Add Geography Chart - Using NIVO

---
+++ src/scenes/geography/index.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import GeographyChart from "../../components/GeographyChart";
+
+const Geography = () => {
+    
+    return (
+        <Box m="20px">
+            <Header title="GEOGRAPHY CHART" subtitle="Simple Geography Chart"/>
+            <Box height="65vh">
+                <GeographyChart />
+            </Box>
+        </Box> 
+    )
+};
+
+export default Geography;
